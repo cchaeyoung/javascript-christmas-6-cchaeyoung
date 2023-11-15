190873fa02d460248241a9338441e5c74a3d5edb
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:59:35 +0900

    refactor: 모듈 내보내기 방식 변경

--- src/InputView.js
+++ src/InputView.js
@@ -1,6 +1,6 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-export default InputView = {
+export default {
     async readDate() {
         const input = await MissionUtils.Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         const date = parseInt(input);
