cc95240916e851261ccc7c10f61b7e50942d1743
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:51:06 +0900

    feat: 주문 메뉴 출력 처리 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,10 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+export default {
+    printMenu(order) {
+        MissionUtils.Console.print("<주문 메뉴>");
+        for (const menu in order) {
+          MissionUtils.Console.print(`${menu} ${order[menu]}개`);
+        }
+    },
 }
