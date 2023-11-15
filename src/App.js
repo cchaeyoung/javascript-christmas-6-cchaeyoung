11681822e4323ea1175853732c76a562f368c2b5
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:43:06 +0900

    feat: 주문과 할인 관련 함수 추가

--- src/App.js
+++ src/App.js
@@ -120,3 +120,23 @@ function calculateEventBadge(totalBenefit) {
 
   return eventBadge;
 }
+
+// 메뉴의 가격을 반환하는 함수
+function getPrice(menu) {
+  const menuPrices = {
+    "양송이수프": 6000,
+    "타파스": 5500,
+    "시저샐러드": 8000,
+    "티본스테이크": 55000,
+    "바비큐립": 54000,
+    "해산물파스타": 35000,
+    "크리스마스파스타": 25000,
+    "초코케이크": 15000,
+    "아이스크림": 5000,
+    "제로콜라": 3000,
+    "레드와인": 60000,
+    "샴페인": 25000,
+  };
+
+  return menuPrices[menu];
+}
