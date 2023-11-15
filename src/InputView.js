44f3f835bb81ee0d0d15519c69aadb0061014bf9
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:44:59 +0900

    feat: 방문 날짜 입력 처리 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,14 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
 export default InputView = {
     async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
+        const input = await MissionUtils.Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        const date = parseInt(input);
+    
+        if (isNaN(date) || date < 1 || date > 31) {
+          throw new Error("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    
+        return date;
+      },
 }
