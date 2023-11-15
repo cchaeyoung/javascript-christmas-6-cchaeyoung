25be79cfd08a1da1d2cae6f618324994377baa6a
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:51:45 +0900

    feat: 할인 전 총주문 금액 출력 처리 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -7,4 +7,9 @@ export default {
           MissionUtils.Console.print(`${menu} ${order[menu]}개`);
         }
     },
+
+    printTotalOrderAmount(discountAmount) {
+        MissionUtils.Console.print("<할인 전 총주문 금액>");
+        MissionUtils.Console.print(`${discountAmount}원`);
+    },
 }
