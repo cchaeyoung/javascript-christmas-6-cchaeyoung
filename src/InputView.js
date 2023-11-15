863e23801fca57a329e8386c6097c19bed72445f
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:47:42 +0900

    feat: 주문 메뉴 입력 처리 추가

--- src/InputView.js
+++ src/InputView.js
@@ -10,5 +10,23 @@ export default InputView = {
         }
     
         return date;
-      },
+    },
+
+    async readMenu() {
+        const input = await MissionUtils.Console.readLineAsync("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        const orderList = input.split(",").map((item) => item.trim());
+    
+        const order = {};
+        for (const item of orderList) {
+          const [menu, count] = item.split("-").map((item) => item.trim());
+    
+          if (!menu || !count || isNaN(parseInt(count))) {
+            throw new Error("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+          }
+    
+          order[menu] = parseInt(count);
+        }
+    
+        return order;
+    },
 }
