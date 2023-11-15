f0b62866800ad5075520837794c0ba38865c1989
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:35:56 +0900

    feat: 주문과 할인 관련 함수 추가

--- src/App.js
+++ src/App.js
@@ -20,4 +20,101 @@ class App {
   }
 }
 
+// 주문의 할인 금액을 계산하는 함수
+function calculateDiscountAmount(order) {
+  let discountAmount = 0;
+
+  for (const menu in order) {
+    const count = order[menu];
+    const price = getPrice(menu);
+
+    discountAmount += price * count;
+  }
+
+  return discountAmount;
+}
+
+// 총 할인 금액을 계산하는 함수
+function calculateTotalDiscount(discountAmount) {
+  let totalDiscount = 0;
+
+  for (let i = 1; i <= 25; i++) {
+    totalDiscount += i * 100;
+  }
+
+  totalDiscount += calculateWeekdayDiscount(discountAmount);
+  totalDiscount += calculateWeekendDiscount(discountAmount);
+
+  return totalDiscount;
+}
+
+// 평일 할인 금액을 계산하는 함수
+function calculateWeekdayDiscount(discountAmount) {
+  const weekdayDiscount = 2023 * 2;
+  const dessertPrice = getPrice("초코케이크");
+  const dessertCount = Math.floor(discountAmount / dessertPrice);
+
+  return weekdayDiscount * dessertCount;
+}
+
+// 주말 할인 금액을 계산하는 함수
+function calculateWeekendDiscount(discountAmount) {
+  const weekendDiscount = 2023 * 2;
+  const mainPrice = getPrice("티본스테이크");
+  const mainCount = Math.floor(discountAmount / mainPrice);
+
+  return weekendDiscount * mainCount;
+}
+
+// 증정 메뉴를 계산하는 함수
+function calculateGiftMenu(totalDiscount) {
+  const giftMenu = {
+    menu: "없음",
+    price: 0,
+  };
+
+  if (totalDiscount >= 120000) {
+    giftMenu.menu = "샴페인";
+    giftMenu.price = getPrice("샴페인");
+  }
+
+  return giftMenu;
+}
+
+// 총 혜택 금액을 계산하는 함수
+function calculateTotalBenefit(discountAmount, giftMenu) {
+  return discountAmount + giftMenu.price;
+}
+
+// 할인 후 최종 결제 금액을 계산하는 함수
+function calculateFinalPayment(order, totalBenefit) {
+  let finalPayment = 0;
+
+  for (const menu in order) {
+    const count = order[menu];
+    const price = getPrice(menu);
+
+    finalPayment += price * count;
+  }
+
+  finalPayment -= totalBenefit;
+
+  return finalPayment;
+}
+
+// 이벤트 배지를 계산하는 함수
+function calculateEventBadge(totalBenefit) {
+  let eventBadge = "없음";
+
+  if (totalBenefit >= 20000) {
+    eventBadge = "산타";
+  } else if (totalBenefit >= 10000) {
+    eventBadge = "트리";
+  } else if (totalBenefit >= 5000) {
+    eventBadge = "별";
+  }
+
+  return eventBadge;
+}
+
 export default App;
