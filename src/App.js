6022f15200f6277f8266271f2f5ede1610a58ef2
cchaeyoung <kimchaeyoung05@naver.com>
2023-11-15 23:34:46 +0900

    feat: 주문과 할인 로직 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ class App {
   async run() {
     const visitDate = await InputView.readDate();
     const order = await InputView.readMenu();
+
+    const discountAmount = calculateDiscountAmount(order);
+    const totalDiscount = calculateTotalDiscount(discountAmount);
+    const giftMenu = calculateGiftMenu(totalDiscount);
+    const totalBenefit = calculateTotalBenefit(discountAmount, giftMenu);
+    const finalPayment = calculateFinalPayment(order, totalBenefit);
+    const eventBadge = calculateEventBadge(totalBenefit);
+
+    OutputView.printMenu(order);
+    OutputView.printTotalOrderAmount(discountAmount);
+    OutputView.printGiftMenu(giftMenu);
+    OutputView.printBenefitDetails(discountAmount, giftMenu);
+    OutputView.printTotalBenefitAmount(totalBenefit);
+    OutputView.printFinalPayment(finalPayment);
+    OutputView.printEventBadge(eventBadge);
   }
 }
 
